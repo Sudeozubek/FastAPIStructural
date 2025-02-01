ca63fd5199eeb20a9dbee19c747b1eac69e61de1
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-02-01 18:48:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,24 @@ class Course:
         self.rating = rating
         self.published_date = published_date
 
+class CourseRequest(BaseModel):
+    id: Optional[int] = Field(description='ID is not required', default=None)
+    title: str = Field(min_length=3)
+    instructor: str = Field(min_length=3)
+    rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "course title",
+                "instructor": "atil samancioglu",
+                "rating": 5,
+                'published_date': 2027
+            }
+        }
+    }
+
 course_db = [
     Course(id= 1, title= "Python", instructor="Sude", rating=5, published_date=2029),
     Course(id= 2, title= "Kotlin", instructor= "Halil", rating= 5, published_date= 2030),
@@ -56,3 +74,36 @@ async def get_courses_by_publish_date(publish_date: int = Query(gt=2005, lt=2036
             courses_to_return.append(course)
 
     return courses_to_return
+
+@app.post("/create-course", status_code=status.HTTP_201_CREATED)
+async def create_course(course_request: CourseRequest):
+    new_course = Course(**course_request.model_dump())
+    courses_db.append(find_course_id(new_course))
+
+
+def find_course_id(course: Course):
+    course.id = 1 if len(courses_db) == 0 else courses_db[-1].id + 1
+    return course
+
+
+@app.put("/courses/update_course", status_code=status.HTTP_204_NO_CONTENT)
+async def update_course(course: CourseRequest):
+    course_changed = False
+    for i in range(len(courses_db)):
+        if courses_db[i].id == course.id:
+            courses_db[i] = course
+            course_changed = True
+    if not course_changed:
+        raise HTTPException(status_code=404, detail='Course not found')
+
+
+@app.delete("/courses/{course_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_course(course_id: int = Path(gt=0)):
+    course_changed = False
+    for i in range(len(courses_db)):
+        if courses_db[i].id == course_id:
+            courses_db.pop(i)
+            course_changed = True
+            break
+    if not course_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
